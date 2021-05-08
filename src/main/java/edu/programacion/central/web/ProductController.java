37503e3469765cdf3a7f20c5ff6903fd5e010e00
lucarpio <christian_carpio1@usmp.pe>
2021-05-08 17:05:41 -0500

    deploy #3

--- src/main/java/edu/programacion/central/web/ProductController.java
+++ src/main/java/edu/programacion/central/web/ProductController.java
@@ -44,58 +44,55 @@ public class ProductController {
         return product;
     }
 
-    @GetMapping(path = { "/products"})
+    @GetMapping(path = { "/products" })
     public String saludar(Model model) {
         model.addAttribute("products", getProducts());
         return "index";
     }
-    
-    @GetMapping(path = {"/poloQuick001"})
-    public String mostrar1(Model model){
+
+    @GetMapping(path = { "/poloQuick001" })
+    public String mostrar1(Model model) {
         return "poloQuick001";
     }
 
-    @GetMapping(path = {"/poloFox001"})
-    public String mostrar2(Model model){
+    @GetMapping(path = { "/poloFox001" })
+    public String mostrar2(Model model) {
         return "poloFox001";
     }
 
-    @GetMapping(path = {"/poloDunkelvok001"})
-    public String mostrar3(Model model){
+    @GetMapping(path = { "/poloDunkelvok001" })
+    public String mostrar3(Model model) {
         return "poloDunkelvok001";
     }
 
-    @GetMapping(path = {"/teclado"})
-    public String mostrar4(Model model){
+    @GetMapping(path = { "/teclado" })
+    public String mostrar4(Model model) {
         return "teclado";
     }
 
-    @GetMapping(path = {"/laptop002"})
-    public String mostrar5(Model model){
+    @GetMapping(path = { "/laptop002" })
+    public String mostrar5(Model model) {
         return "laptop001";
     }
 
-    @GetMapping(path = {"/mouse001"})
-    public String mostrar6(Model model){
+    @GetMapping(path = { "/mouse001" })
+    public String mostrar6(Model model) {
         return "mouse001";
     }
-
-    
-
 
     // @GetMapping(path= {"/public"}) public ModelAndView post(){ ModelAndView
     // modelAndView = new ModelAndView(Pagina.HOME); modelAndView.addObject("posts",
     // getPosts()); return modelAndView; }
-    
 
     // @GetMapping(path = { "/products" })
-    // public ModelAndView getPostIndividual(@RequestParam(defaultValue = "1", name = "id", required = false) int id) {
-    //     ModelAndView modelAndView = new ModelAndView(Pagina.Post);
-    //     List<Post> postFiltrado = this.getPosts().stream().filter((p) -> {
-    //         return p.getId() == id;
-    //     }).collect(Collectors.toList());
-    //     modelAndView.addObject("post", postFiltrado.get(0));
-    //     return modelAndView;
+    // public ModelAndView getPostIndividual(@RequestParam(defaultValue = "1", name
+    // = "id", required = false) int id) {
+    // ModelAndView modelAndView = new ModelAndView(Pagina.Post);
+    // List<Post> postFiltrado = this.getPosts().stream().filter((p) -> {
+    // return p.getId() == id;
+    // }).collect(Collectors.toList());
+    // modelAndView.addObject("post", postFiltrado.get(0));
+    // return modelAndView;
     // }
 
 }
