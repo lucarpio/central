1b8abc2284a36381b814994b5954b582c6f7d44a
Alvaro Neira <alvaroneira98@gmail.com>
2021-05-18 16:00:04 -0500

    ProductoRepository

--- src/main/java/edu/programacion/central/repository/ProductoRepository.java
+++ src/main/java/edu/programacion/central/repository/ProductoRepository.java
@@ -1,11 +1,12 @@
 package edu.programacion.central.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import edu.programacion.central.domain.Producto;
 
 @Repository
-public interface ProductoRepository extends CrudRepository<Producto, Long>{
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
     
 }
