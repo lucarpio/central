0144462e3913bcd14dc68e859145bc5121f92521
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-05-29 15:51:59 -0500

    Refactor Persona Repository

--- src/main/java/edu/programacion/central/repository/PersonaRepository.java
+++ src/main/java/edu/programacion/central/repository/PersonaRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.programacion.central.model.Persona;
 
-public interface IPersonaRepo extends JpaRepository<Persona, Integer>{
+public interface PersonaRepository extends JpaRepository<Persona, Integer>{
     
 }
