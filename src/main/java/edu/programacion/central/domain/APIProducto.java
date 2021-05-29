b2e8ecd5f77e4921d4ec492089d428afcb0485a7
lucarpio <christian_carpio1@usmp.pe>
2021-05-29 11:54:25 -0500

    test backend APIs

---
+++ src/main/java/edu/programacion/central/domain/APIProducto.java
@@ -0,0 +1,78 @@
+package edu.programacion.central.domain;
+
+public class APIProducto {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private String categoria;
+    private String imagen;
+    private Double precio;
+
+    public APIProducto() {
+    }
+
+    public APIProducto(Integer id, String nombre, String descripcion, String categoria, String imagen, Double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.imagen = imagen;
+        this.precio = precio;
+    }
+    public APIProducto(String nombre, String descripcion, String categoria, String imagen, Double precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.imagen = imagen;
+        this.precio = precio;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getImagen() {
+        return this.imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public Double getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+}
